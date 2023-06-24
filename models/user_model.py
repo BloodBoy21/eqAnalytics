0099244dcc933b6656725da8bccfbe8742070901
Alan B <death1027@outlook.com>
2023-06-24 15:41:00 -0600

    refactor: :recycle: update user db model

--- models/user_model.py
+++ models/user_model.py
@@ -6,7 +6,5 @@ class User(db):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(String)
-    email = Column(String)
-    country_code = Column(String, index=True)
     lat = Column(Float)
     lon = Column(Float)
