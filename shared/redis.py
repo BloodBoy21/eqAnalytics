bbd2832bd7173f1e074a14c12bff995216b9837c
Alan B <death1027@outlook.com>
2023-06-25 12:15:53 -0600

    feat: :recycle: add dotenv

--- shared/redis.py
+++ shared/redis.py
@@ -1,6 +1,7 @@
 import redis
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
 REDIS_DB = os.environ.get("REDIS_DB", 0)
